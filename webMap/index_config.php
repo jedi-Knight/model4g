a47bb944e0c78edb3cc0903ed4f9021c8ad71ef4
jediKnight <gautamxpratik@gmail.com>
2014-12-14 01:50:07 +0545

    general pois map added;

--- webMap/index_config.php
+++ webMap/index_config.php
@@ -1,3 +1,3 @@
 <?php
-$index_config = json_decode('{"map-init":{"schools":"js/config/schools.js", "colleges": "js/config/colleges.js", "hospitals":"js/config/hospitals.js"}}', true);
+$index_config = json_decode('{"map-init":{"schools":"js/config/schools.js", "colleges": "js/config/colleges.js", "hospitals":"js/config/hospitals.js", "general":"js/config/generalpois.js"}}', true);
 ?>
---
+++ webMap/js/config/generalpois.js
@@ -0,0 +1,116 @@
+config = {
+    /*api: {
+        url: "data/schools.geojson",
+        type: "GET"
+    },*/
+    otherAPIs: {
+        overpass: {
+            url: "."
+        }
+    },
+    "map-of": "General POIs",
+    "data-src": "generalpois.geojson"
+};
+
+
+var Styles = {
+    polygonStyle: {
+        color: "#333366",
+        weight: 1,
+        opacity: 0.9,
+        fillColor: function() {
+            //return svg pattern urls..
+            return "#000000"; //temporary/fallback fill color..
+        }(),
+        fillOpacity: 1
+    },
+    lineStyle: {
+        color: "#333366",
+        weight: 3,
+        opacity: 0.9,
+        fillColor: "#000",
+        fillOpacity: 0
+    },
+    iconStyle: {
+        iconSize: [25, 42],
+        iconAnchor: [12.5, 40],
+        html: "<img src='../markers/flag_orange.png'/>"
+
+    }
+};
+
+var PlugsForStyling = {
+    popup: {
+        body: {
+            "head-plug": "<div class='head-plug'/>"
+        }
+    }
+};
+
+function PanelDocumentModel(pointAttributes) {
+
+    return {titleBarJson: {
+            /*"title": pointAttributes.name + ", " + pointAttributes.city
+             ,"slider": new UI_ThumbnailView({
+             thumbUrls: function() {
+             var srcs = [];
+             for (var photo in pointAttributes.pictures) {
+             //                        srcs.push("data/media/photos/thumbs/" + pointAttributes.pictures[photo]["pictures/photo"]);
+             srcs.push("https://raw.githubusercontent.com/biplovbhandari/brckkln/master/media/photos/thumbs/" + pointAttributes.pictures[photo]["pictures/photo"]);
+             }
+             return srcs;
+             }(),
+             photoUrls: function() {
+             var srcs = [];
+             for (var photo in pointAttributes.pictures) {
+             //                        srcs.push("data/media/photos/" + pointAttributes.pictures[photo]["pictures/photo"]);
+             srcs.push("https://raw.githubusercontent.com/biplovbhandari/brckkln/master/media/photos/" + pointAttributes.pictures[photo]["pictures/photo"]);
+             }
+             return srcs;
+             }(),
+             mediaOptions: function(params) {
+             return {
+             triggers: {
+             click: function(e) {
+             //                                new MediaDocument(params.src).appendTo($("#map").find(".leaflet-popup-pane"));
+             new SplashScreen(MediaDocument(params.src)).appendTo("body");
+             }
+             }
+             };
+             }
+             }).createSlider()*/
+        },
+        headerJson: {
+            "": pointAttributes.name
+//            "Contact Person": pointAttributes.contact_person,
+//            "Contact Number": pointAttributes.contact_number
+//                ,"city": pointAttributes.city
+        },
+        tabsJson: {
+            triggers: {
+                title: function() {
+
+                }
+            },
+            tabs: [
+                {
+                    title: "General Information",
+                    content: {
+                        //"Level": pointAttributes.isced_leve,
+                        //"Operator": pointAttributes.operator_t,
+                        //"Number of Students": pointAttributes.student_co,
+                        //"Number of School Staff": pointAttributes.personnel_,
+                        //"Phone Number": pointAttributes.phone
+                    }
+                }
+            ]
+        },
+        documentModel: {
+            titleBar: {
+                title: "Summary",
+                cotrols: new UI_CloseButton()
+            }
+        }
+    };
+
+}
